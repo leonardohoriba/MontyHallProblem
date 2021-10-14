8c6ac00f54f886906f784d7c18d3ae58ea366a51
GUSTAVO MARIN GOULART <gustavomaringoulart@gmail.com>
2021-10-13 23:52:31 -0300

    server side basic comunication

---
+++ server/main.py
@@ -0,0 +1,42 @@
+import socket 
+
+# while True:
+#     data=conn.recv(1024)
+#     if not data:
+#         print('Fechando conexão')
+#         conn.close()
+#         break
+#     conn.sendall(str.encode(str(data)))
+
+class server():
+    HOST = 'localhost'
+    PORT = 50000
+
+    def __init__(self):
+        #Utilizaremos o protocolo tcp para maior confiabilidade da conexão 
+        self.s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.bind((self.HOST,self.PORT))
+        self.s.listen()
+        print('Aguardando a conexão de um cliente')
+        print('conectado em ', self.ender)
+        
+
+    def receber(self):
+        self.conn,self.ender=self.s.accept()
+        data=self.conn.recv(1024)
+        self.conn.sendall(str.encode(str(data)))
+        
+    def abrir(self):
+        self.s.listen()
+        print('conectado em ', self.ender)
+
+    def fechar (self):
+        print('Fechando conexão')
+        self.conn.close()
+
+
+servidor= server()
+servidor.receber()
+servidor.fechar()
+servidor.abrir()
+#servidor.receber()
