928307dab9638f03de3bd8308b66dc9f20600040
Leonardo Horiba <leonardohoriba@gmail.com>
2021-10-21 12:18:35 -0300

    response_skeleton

--- Client/main.py
+++ Client/main.py
@@ -1,5 +1,6 @@
 import pygame
 import os
+import random
 from pygame.locals import *
 from sys import exit
 pygame.font.init()
@@ -18,19 +19,26 @@ GOAT_WIDTH, GOAT_HEIGHT = (DOOR_HEIGHT, DOOR_WIDTH)
 
 # **== POSITIONS ==**
 POS_TITLE = (10, 10)
-POS_DOOR_1 = ((WIDTH - DOOR_WIDTH)/2 - 250, 100)
-POS_DOOR_2 = ((WIDTH - DOOR_WIDTH)/2, 100)
-POS_DOOR_3 = ((WIDTH - DOOR_WIDTH)/2 + 250, 100)
-POS_DOOR_OPENED_1 = ((WIDTH - DOOR_WIDTH)/2 - 350, 100)
-POS_DOOR_OPENED_2 = ((WIDTH - DOOR_WIDTH)/2 - 100, 100)
-POS_DOOR_OPENED_3 = ((WIDTH - DOOR_WIDTH)/2 + 150, 100)
-POS_CAR_1 = ((WIDTH - DOOR_WIDTH)/2 - 350, 50)
-POS_CAR_2 = ((WIDTH - DOOR_WIDTH)/2 - 100, 50)
-POS_CAR_3 = ((WIDTH - DOOR_WIDTH)/2 + 150, 50)
-POS_GOAT_1 = ((WIDTH - DOOR_WIDTH)/2 - 300, 130)
-POS_GOAT_2 = ((WIDTH - DOOR_WIDTH)/2 - 50, 130)
-POS_GOAT_3 = ((WIDTH - DOOR_WIDTH)/2 + 200, 130)
-
+POS_DOOR_1 = ((WIDTH - DOOR_WIDTH)/2 - 250, 130)
+POS_DOOR_2 = ((WIDTH - DOOR_WIDTH)/2, 130)
+POS_DOOR_3 = ((WIDTH - DOOR_WIDTH)/2 + 250, 130)
+POS_DOOR_OPENED_1 = ((WIDTH - DOOR_WIDTH)/2 - 350, 130)
+POS_DOOR_OPENED_2 = ((WIDTH - DOOR_WIDTH)/2 - 100, 130)
+POS_DOOR_OPENED_3 = ((WIDTH - DOOR_WIDTH)/2 + 150, 130)
+POS_CAR_1 = ((WIDTH - DOOR_WIDTH)/2 - 350, 120)
+POS_CAR_2 = ((WIDTH - DOOR_WIDTH)/2 - 100, 120)
+POS_CAR_3 = ((WIDTH - DOOR_WIDTH)/2 + 150, 120)
+POS_GOAT_1 = ((WIDTH - DOOR_WIDTH)/2 - 270, 120)
+POS_GOAT_2 = ((WIDTH - DOOR_WIDTH)/2 - 20, 120)
+POS_GOAT_3 = ((WIDTH - DOOR_WIDTH)/2 + 230, 120)
+POS_STAY_1 = (POS_DOOR_1[0] + DOOR_WIDTH/2 - 25, POS_DOOR_1[1] + DOOR_HEIGHT/2)
+POS_STAY_2 = (POS_DOOR_2[0] + DOOR_WIDTH/2 - 25, POS_DOOR_2[1] + DOOR_HEIGHT/2)
+POS_STAY_3 = (POS_DOOR_3[0] + DOOR_WIDTH/2 - 25, POS_DOOR_3[1] + DOOR_HEIGHT/2)
+POS_SWITCH_1 = (POS_DOOR_1[0] + DOOR_WIDTH/2 - 40, POS_DOOR_1[1] + DOOR_HEIGHT/2)
+POS_SWITCH_2 = (POS_DOOR_2[0] + DOOR_WIDTH/2 - 40, POS_DOOR_2[1] + DOOR_HEIGHT/2)
+POS_SWITCH_3 = (POS_DOOR_3[0] + DOOR_WIDTH/2 - 40, POS_DOOR_3[1] + DOOR_HEIGHT/2)
+POS_STAYS = [POS_STAY_1, POS_STAY_2, POS_STAY_3]
+POS_SWITCHES = [POS_SWITCH_1, POS_SWITCH_2, POS_SWITCH_3]
 # **== CONFIG ==**
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Monty Hall Problem")
@@ -88,16 +96,23 @@ class Button():
 def main():
     clock = pygame.time.Clock()
     run = True
-    main_font = pygame.font.SysFont("comicsans", 50)
-    title = main_font.render("Monty Hall Problem", 1, (0,0,0))
 
-    reset_button = Button((102, 61, 16), 300, 400, BUTTON_WIDTH, BUTTON_HEIGHT, 'RESET')
+    main_font = pygame.font.SysFont("comicsans", 50)
+    secondary_font = pygame.font.SysFont("comicsans", 30)
+    mario_font = pygame.font.Font(os.path.join('Mario-Kart-DS.ttf'), 55)
+
+    title = mario_font.render("MONTY HALL PROBLEM", 1, (0,0,0))
+    stay = secondary_font.render("Stay", 1, (0,0,0))
+    switch = secondary_font.render("Switch", 1, (0,0,0))
+
+
+    reset_button = Button((102, 61, 16), 20, HEIGHT - 50, BUTTON_WIDTH, BUTTON_HEIGHT, 'RESET')
     door_1 = Image('Assets', 'door.png', POS_DOOR_1[0], POS_DOOR_1[1], DOOR_WIDTH, DOOR_HEIGHT)
     door_2 = Image('Assets', 'door.png', POS_DOOR_2[0], POS_DOOR_2[1], DOOR_WIDTH, DOOR_HEIGHT)
     door_3 = Image('Assets', 'door.png', POS_DOOR_3[0], POS_DOOR_3[1], DOOR_WIDTH, DOOR_HEIGHT)
-    door_opened_1 = Image('Assets', 'door_opened.png', POS_DOOR_OPENED_1[0], POS_DOOR_OPENED_1[1], DOOR_OPENED_WIDTH, DOOR_OPENED_WIDTH)
-    door_opened_2 = Image('Assets', 'door_opened.png', POS_DOOR_OPENED_2[0], POS_DOOR_OPENED_2[1], DOOR_OPENED_WIDTH, DOOR_OPENED_WIDTH)
-    door_opened_3 = Image('Assets', 'door_opened.png', POS_DOOR_OPENED_3[0], POS_DOOR_OPENED_3[1], DOOR_OPENED_WIDTH, DOOR_OPENED_WIDTH)
+    door_opened_1 = Image('Assets', 'door_opened.png', POS_DOOR_OPENED_1[0], POS_DOOR_OPENED_1[1], DOOR_OPENED_WIDTH, DOOR_OPENED_HEIGHT)
+    door_opened_2 = Image('Assets', 'door_opened.png', POS_DOOR_OPENED_2[0], POS_DOOR_OPENED_2[1], DOOR_OPENED_WIDTH, DOOR_OPENED_HEIGHT)
+    door_opened_3 = Image('Assets', 'door_opened.png', POS_DOOR_OPENED_3[0], POS_DOOR_OPENED_3[1], DOOR_OPENED_WIDTH, DOOR_OPENED_HEIGHT)
     car_1 = Image('Assets', 'car.png', POS_CAR_1[0], POS_CAR_1[1], CAR_WIDTH, CAR_WIDTH)
     car_2 = Image('Assets', 'car.png', POS_CAR_2[0], POS_CAR_2[1], CAR_WIDTH, CAR_WIDTH)
     car_3 = Image('Assets', 'car.png', POS_CAR_3[0], POS_CAR_3[1], CAR_WIDTH, CAR_WIDTH)
@@ -105,32 +120,59 @@ def main():
     goat_2 = Image('Assets', 'goat.png', POS_GOAT_2[0], POS_GOAT_2[1], GOAT_WIDTH, GOAT_WIDTH)
     goat_3 = Image('Assets', 'goat.png', POS_GOAT_3[0], POS_GOAT_3[1], GOAT_WIDTH, GOAT_WIDTH)
 
-
+    doors = [door_1, door_2, door_3]
+    doors_opened = [door_opened_1, door_opened_2, door_opened_3]
+    cars = [car_1, car_2, car_3]
+    goats = [goat_1, goat_2, goat_3]
+    
     # **== Renderização das figuras, em ordem de profundidade ==**
     def render():
         WIN.fill(BACKGROUND_COLOR)
         # WIN.blit(BACKGROUND, (0,0))
-        WIN.blit(title, (10, 10))
-        door_1.draw()
-        door_2.draw()
-        door_3.draw()
-        door_opened_1.draw()
-        door_opened_2.draw()
-        door_opened_3.draw()
-        car_1.draw()
-        car_2.draw()
-        car_3.draw()
-        goat_1.draw()
-        goat_2.draw()
-        goat_3.draw()
+        WIN.blit(title, (WIDTH/7, 30))
+
+        # door_1.draw()
+        # door_2.draw()
+        # # door_3.draw()
+        # # door_opened_1.draw()
+        # # door_opened_2.draw()
+        # door_opened_3.draw()
+        # # car_1.draw()
+        # # car_2.draw()
+        # # car_3.draw()
+        # # goat_1.draw()
+        # # goat_2.draw()
+        # goat_3.draw()
+
+        response = {
+            'position' : ['d', 'd', 'g'],
+            'stay': 1,
+            'switch': 2
+        }
+        for idx, elem in enumerate(response['position']):
+            if elem == 'd':
+                doors[idx].draw()
+            elif elem == 'g':
+                doors_opened[idx].draw()
+                goats[idx].draw()
+            elif elem == 'c':
+                doors_opened[idx].draw()
+                cars[idx].draw()
+            if response['stay']:
+                WIN.blit(stay, POS_STAYS[response['stay'] - 1])
+            if response['switch']:
+                WIN.blit(switch, POS_SWITCHES[response['switch'] - 1])
+
+
         reset_button.draw(WIN, (0,0,0))
+        
+
         pygame.display.flip() 
         # pygame.display.update() #flip melhor que o update em processamento
-
+    
     while run:
         clock.tick(FPS)
         render()
-
 
         for event in pygame.event.get():
             mouse_pos = pygame.mouse.get_pos()
@@ -141,7 +183,20 @@ def main():
                 # Button Clicks
                 if reset_button.isOver(mouse_pos): 
                     print(mouse_pos)
+                if door_1.isOver(mouse_pos): 
+                    request = 0
+                    print(request)
+                    # function send request to server
+                if door_2.isOver(mouse_pos): 
+                    request = 1
+                    print(request)
+                    # function send request to server
+                if door_3.isOver(mouse_pos): 
+                    request = 2
+                    print(request)
+                    # function send request to server
                 
+ 
             if event.type == pygame.MOUSEMOTION:
                 # Button Motion
                 if reset_button.isOver(mouse_pos):
