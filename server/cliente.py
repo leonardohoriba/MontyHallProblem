8a3dd12c4f19d90c70f32558487bb9b1988f3905
conexa@saude <gustavo.goulart@conexasaude.com.br>
2021-10-21 12:58:41 -0300

    new server with functions

--- server/cliente.py
+++ server/cliente.py
@@ -1,12 +1,44 @@
-import socket 
+import socket
+import threading
+import time
 
-HOST = '127.0.0.1'
-PORT = 50000 
 
-s= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.connect((HOST,PORT))
-s.sendall(str.encode('8'))
 
-data=s.recv(1024)
+class Client():
+    PORT = 5050
+    FORMATO = 'utf-8'
+    SERVER = "127.0.14.1"
+    ADDR = (SERVER, PORT)
 
-print('Mensagem:', data.decode())
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.client.connect(self.ADDR)
+        thread1 = threading.Thread(target=self.handle_mensagens)
+        thread2 = threading.Thread(target=self.iniciar_envio)
+        thread1.start()
+        thread2.start()
+            
+    
+    def handle_mensagens(self):
+        while(True):
+            msg = self.client.recv(1024).decode()
+            print('\n'+msg)
+    
+    def enviar_mensagem(self):
+        mensagem = input("digite sua jogada")
+        self.enviar("msg=" + mensagem)
+
+    def enviar(self,mensagem):
+        print("enviado")
+        self.client.send(str(mensagem).encode(self.FORMATO))
+
+    def iniciar_envio(self):
+        while(True):
+            self.enviar_mensagem()
+
+
+
+
+
+
+Client()
--- server/main.py
+++ server/main.py
@@ -1,34 +1,56 @@
-import socket 
+import socket
+import threading
+import time
 
-class server():
-    HOST = 'localhost'
-    PORT = 50000
+
+
+class Server():
+    conexoes = {}
+    mensagens = []
+    #socket do tipo TCP socket.SOCK_STREAM
+    SERVER_IP = '127.0.14.1'
+    #socket.gethostbyname(socket.gethostname())
+    PORT = 5050
+    ADDR = (SERVER_IP, PORT)
+    FORMATO = 'utf-8'
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(ADDR)
+    conn,addr=False,False
+    run=True
+    nome=False
 
     def __init__(self):
-        #Utilizaremos o protocolo tcp para maior confiabilidade da conexão 
-        self.s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.s.bind((self.HOST,self.PORT))
-        self.s.listen()
-        print('Aguardando a conexão de um cliente')
-        print('conectado em ', self.ender)
+        print(f"[INICIO] Socket iniciado no ip {self.SERVER_IP}")
+        self.server.listen()
+        while (self.run):
+            self.conn, self.addr = self.server.accept()
+            thread = threading.Thread(target=self.handle_clientes, args=(self.conn,self.addr))
+            thread.start()
+
+    def handle_clientes(self,conn,addr):
+        print(f'[NOVA CONEXÃO] Endereço: {addr}')
+        self.nome = False
+
+        while(True):
+            msg= conn.recv(1024).decode()
+            if(msg):
+                if(msg=='msg=exit'):
+                    self.conn.close()
+                print(msg)
+                
+                mensagem_separada= msg.split("=")
+                self.nome = mensagem_separada[1]
+                   
+                mensagem=mensagem_separada[1]
+                self.mensagens.append(mensagem)
+                self.enviar_mensagem(mensagem)
+
+    def enviar_mensagem(self,mensagem):
+        mensagem_envio=mensagem
+        self.conn.send(mensagem_envio.encode(self.FORMATO))
+        time.sleep(0.2)
+
         
 
-    def receber(self):
-        self.conn,self.ender=self.s.accept()
-        data=self.conn.recv(1024)
-        self.conn.sendall(str.encode(str(data)))
-        
-    def abrir(self):
-        self.s.listen()
-        print('conectado em ', self.ender)
 
-    def fechar (self):
-        print('Fechando conexão')
-        self.conn.close()
-
-
-servidor= server()
-servidor.receber()
-servidor.fechar()
-servidor.abrir()
-#servidor.receber()
+Server()
