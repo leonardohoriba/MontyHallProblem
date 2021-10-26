69ef63e84aa8be5a9e9c898ada7dc81fa6c6a163
GUSTAVO MARIN GOULART <gustavomaringoulart@gmail.com>
2021-10-26 13:08:00 -0300

    funcionando metodo de enviar mensagem

--- server/cliente.py
+++ server/cliente.py
@@ -1,5 +1,4 @@
 import socket
-import threading
 import time
 
 
@@ -13,41 +12,29 @@ class Client():
     def __init__(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.client.connect(self.ADDR)
-        thread1 = threading.Thread(target=self.handle_mensagens)
-        thread2 = threading.Thread(target=self.iniciar_envio)
-        thread1.start()
-        thread2.start()
-            
     
-    def handle_mensagens(self):
-        #while(True):
-            msg = self.client.recv(1024).decode()
-            print('\n'+msg)
-    
-    def enviar_mensagem(self):
-        mensagem = input("digite sua jogada\n")
-        self.enviar("msg=" + mensagem)
 
-    def enviar(self,mensagem):
-        print("enviado")
+
+    def jogar(self,mensagem):
         self.client.send(str(mensagem).encode(self.FORMATO))
+        print("enviado")
         msg = None
         while msg is None:
             msg = self.client.recv(1024).decode()
         return msg
 
 
-    def iniciar_envio(self):
-        while(True):
-            self.enviar_mensagem()
 
 
 
 
 
 
-Client()
+
+cliente1 = Client()
 while(True):
     msg = input("digite sua mensagem:")
-    resposta = Client.enviar(msg)
-    print("essa é a resposta:" + msg)
+    time.sleep(0.2)
+    resposta = cliente1.jogar("msg=" + msg)
+    time.sleep(0.2)
+    print("essa é a resposta:" + resposta)
