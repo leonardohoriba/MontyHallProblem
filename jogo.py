6a49695bed268c8c0943abf5c6ccce181e028e35
GUSTAVO MARIN GOULART <gustavomaringoulart@gmail.com>
2021-11-01 18:14:03 -0300

    jogo finalizado

--- jogo.py
+++ jogo.py
@@ -1,9 +1,4 @@
-from _typeshed import NoneType
-import socket
-import time
 from random import randrange
-
-
 class game():
     doors=[0,1,2]
     retorno = None
@@ -55,64 +50,15 @@ class game():
             self.vector=["d","d","d"]
             self.vencedor=int(randrange(0,2))
             return str(self.retorno)
+
+jogo = game()
+jogo.jogada(1,1)
+jogo.jogada(1,2)
+jogo.jogada(1,3)
+jogo.jogada(1,1)
+jogo.jogada(0,2)
+jogo.jogada(1,1)
+
+
+
             
-
-
-class Server():
-    conexoes = {}
-    mensagens = []
-    
-    SERVER_IP = '127.0.3.1'
-    #socket.gethostbyname(socket.gethostname())
-    PORT = 5050
-    ADDR = (SERVER_IP, PORT)
-    FORMATO = 'utf-8'
-    #socket do tipo TCP socket.SOCK_STREAM
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind(ADDR)
-    conn,addr=False,False
-    run=True
-    nome=False
-
-    def __init__(self):
-        print(f"[INICIO] Socket iniciado no ip {self.SERVER_IP}")
-        self.server.listen()
-        while (self.run):
-            self.conn, self.addr = self.server.accept()
-            self.handle_clientes(self.conn, self.addr)
-
-    def handle_clientes(self,conn,addr):
-        print(f'[NOVA CONEXÃO] Endereço: {addr}')
-        self.nome = False
-        vencedor=str(randrange(0,2))
-        doors=['door1','door2','door3']
-        print('vencedor = ' + vencedor)
-        reposta = {}
-        status = None
-        while(True):
-            msg= conn.recv(1024).decode()
-            if(msg):
-                if(msg=='msg=exit'):
-                    self.conn.close()
-                print(msg)
-                
-                mensagem_separada= msg.split("=")
-                self.nome = mensagem_separada[1]
-                   
-                mensagem=mensagem_separada[1]
-                retorno = mensagem
-                #JOGO
-                self.enviar_mensagem(retorno)
-
-    def enviar_mensagem(self,mensagem):
-        mensagem_envio=mensagem
-        self.conn.send(mensagem_envio.encode(self.FORMATO))
-        time.sleep(0.2)
-    
-    def fechar_servidor(self):
-        pass
-
-        
-
-
-Server()
